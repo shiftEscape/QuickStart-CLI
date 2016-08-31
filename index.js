6ef948824dac168b7d53686e1a1bf3ebaba66085
Alvin James Bellero <alvinb@zylun.com>
2016-08-31 09:31:52 +0800

    Updated cli option

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 var program = require('commander');
 var mkdirp = require('mkdirp');
+var chalk = require('chalk');
 var fs  = require('fs');
 var fse = require('fs-extra');
+
+var ora = require('ora');
+var cliSpinners = require('cli-spinners');
+var spinner = ora(cliSpinners.dots);
+
+var childProcess = require('child_process');
+var exec = childProcess.exec;
+var execFile = childProcess.execFile;
+
 
 function fsExistsSync(myDir) {
 	try {
@@ -13,12 +23,30 @@ function fsExistsSync(myDir) {
 	}
 }
 
+function runLoading (text) {
+	spinner.start();
+	spinner.color = 'green'
+	spinner.text = chalk.green(text);
+}
+
+function installMods (projectName) {
+	exec("cd " + projectName + " && npm i", function (error, stdout, stderr) {
+		if (stderr) {
+			console.log(chalk.red('Errors: ') + stderr);
+		} else if (error !== null) {
+			console.log(chalk.red('Execution errors: ') + error);
+		}
+		console.log(stdout);
+		spinner.succeed();
+		console.log(chalk.bold.green('Go to your project directory and use `npm start` to run\n'));
+	});
+}
+
 program
-	.version('0.0.1')
-	.option('-c, --create <project name>', 'The project name to be created')
+	.option('-n, --new <project name>', 'The project name to be created')
 	.parse(process.argv);
 
-var projectName = program.create;
+var projectName = program.new;
 
 if (projectName) {
 	if(!fsExistsSync(projectName)) {
@@ -27,7 +55,15 @@ if (projectName) {
 			else {
 				var path = __dirname + '/files/';
 				fse.copy(path, projectName, function (err) {
-					console.log('done');
+					execFile('find', [ projectName ], function(err, stdout, stderr) {
+					  var file_list = stdout.split('\n');
+						file_list.splice(-1, 1);
+						for (var i in file_list) {
+							console.log(chalk.green('created') + ' ' + file_list[i]);
+						} console.log('\n');
+						runLoading('Installing packages for tooling via NPM..');
+						installMods(projectName);
+					});
 				});
 			}
 		});
