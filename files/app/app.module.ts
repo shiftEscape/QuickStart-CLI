b9e4843467c61c1471b1d9f1daa79f104585ff31
Alvin James Bellero <alvinb@zylun.com>
2016-09-03 14:13:07 +0800

    Added providers metadata on default app.module boilerplate

--- files/app/app.module.ts
+++ files/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent }  from './app.component';
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
+  providers:    [ ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
