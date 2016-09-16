e83b09d9f1a3d87e80c320dfa0008f97e4d2c86a
shiftescape <ajames.bellero@gmail.com>
2016-09-17 07:04:07 +0800

    Added providers metadata

--- files/app/app.module.ts
+++ files/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent }   from './app.component';
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers:    [ ]
 })
 export class AppModule { }
